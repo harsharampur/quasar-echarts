662425f8f6a345a5f135861dd474b91466e8d360
Harsha Rampur <135097306+harsharampur@users.noreply.github.com>
2024-10-10 10:51:38 +0530

    Create piechart.py

---
+++ piechart.py
@@ -0,0 +1,19 @@
+plt.pie(df['Age'], labels = {"A", "B", "C", 
+                            "D", "E", "F", 
+                            "G", "H", "I", "J"}, 
+                            
+autopct ='% 1.1f %%', shadow = True) 
+plt.show() 
+
+plt.pie(df['Income'], labels = {"A", "B", "C", 
+                                "D", "E", "F", 
+                                "G", "H", "I", "J"}, 
+                                
+autopct ='% 1.1f %%', shadow = True) 
+plt.show() 
+
+plt.pie(df['Sales'], labels = {"A", "B", "C", 
+                            "D", "E", "F", 
+                            "G", "H", "I", "J"}, 
+autopct ='% 1.1f %%', shadow = True) 
+plt.show() 
